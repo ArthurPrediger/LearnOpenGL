6db4e1e60ed4653b5c60dbe66b91ac99b5d28f43
Arthur Prediger <arthurprediger@edu.unisinos.br>
2024-04-08 00:20:52 -0300

    Using different pbr functions in the pixel shader for a cg class exercise.

--- src/6.pbr/0.0.atv2_cg_avancada/0.0.pbr.fs
+++ src/6.pbr/0.0.atv2_cg_avancada/0.0.pbr.fs
@@ -40,16 +40,14 @@ vec3 getNormalFromMap()
     return normalize(TBN * tangentNormal);
 }
 // ----------------------------------------------------------------------------
-float DistributionGGX(vec3 N, vec3 H, float roughness)
+float DistributionBlinn(vec3 N, vec3 H, float roughness)
 {
     float a = roughness*roughness;
     float a2 = a*a;
     float NdotH = max(dot(N, H), 0.0);
-    float NdotH2 = NdotH*NdotH;
 
-    float nom   = a2;
-    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
-    denom = PI * denom * denom;
+    float nom = pow(NdotH, ((2.0/a2) - 2.0));
+    float denom = PI * a2;
 
     return nom / denom;
 }
@@ -65,19 +63,17 @@ float GeometrySchlickGGX(float NdotV, float roughness)
     return nom / denom;
 }
 // ----------------------------------------------------------------------------
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+float GeometryNeumann(vec3 N, vec3 V, vec3 L, float roughness)
 {
-    float NdotV = max(dot(N, V), 0.0);
     float NdotL = max(dot(N, L), 0.0);
-    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
-    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+    float NdotV = max(dot(N, V), 0.0);
 
-    return ggx1 * ggx2;
+    return (NdotL * NdotV) / max(NdotL, NdotV);
 }
 // ----------------------------------------------------------------------------
 vec3 fresnelSchlick(float cosTheta, vec3 F0)
 {
-    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+    return F0;
 }
 // ----------------------------------------------------------------------------
 void main()
@@ -107,8 +103,8 @@ void main()
         vec3 radiance = lightColors[i] * attenuation;
 
         // Cook-Torrance BRDF
-        float NDF = DistributionGGX(N, H, roughness);   
-        float G   = GeometrySmith(N, V, L, roughness);      
+        float NDF = DistributionBlinn(N, H, roughness);   
+        float G   = GeometryNeumann(N, V, L, roughness);      
         vec3 F    = fresnelSchlick(max(dot(H, V), 0.0), F0);
            
         vec3 numerator    = NDF * G * F; 
